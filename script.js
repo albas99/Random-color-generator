288b49f09acabad8472ae0f91c776fe01104cbfb
Faisal Albasu <albasfaisal@yandex.ru>
2020-12-28 21:13:57 +0400

    Random color generator

---
+++ script.js
@@ -0,0 +1,38 @@
+
+randomColor1 = () => {
+    let color = Math.floor(Math.random()*16777215).toString(16);
+
+    document.querySelector("#container-1").style.backgroundColor = "#" + color;
+
+    color.innerHTML = "#" + color;
+}
+
+randomColor2 = () => {
+    let color = Math.floor(Math.random()*16777215).toString(16);
+
+    document.querySelector("#container-2").style.backgroundColor = "#" + color;
+
+    color.innerHTML = "#" + color;
+}
+randomColor3 = () => {
+    let color = Math.floor(Math.random()*16777215).toString(16);
+
+    document.querySelector("#container-3").style.backgroundColor = "#" + color;
+
+    color.innerHTML = "#" + color;
+}
+randomColor4 = () => {
+    let color = Math.floor(Math.random()*16777215).toString(16);
+
+    document.querySelector("#container-4").style.backgroundColor = "#" + color;
+
+    color.innerHTML = "#" + color;
+}
+randomColor5 = () => {
+    let color = Math.floor(Math.random()*16777215).toString(16);
+
+    document.querySelector("#container-5").style.backgroundColor = "#" + color;
+
+    color.innerHTML = "#" + color;
+}
+
